dae4eac94895af5a3a889d32e151c5e3392297ed
EstebanGD312 <estebangliceo@gmail.com>
2026-07-15 08:46:48 -0500

    Add MVC

---
+++ Esteban/frontend/Models/Producto.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace GestionProductos.Models
+{
+    public class Producto
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "El nombre es obligatorio.")]
+        [StringLength(50, MinimumLength = 3, ErrorMessage = "El nombre debe tener entre 3 y 50 caracteres.")]
+        [Display(Name = "Nombre")]
+        public string Nombre { get; set; }
+
+        [Required(ErrorMessage = "El precio es obligatori.")]
+        [Display(Name = "Precio")]
+        public int Precio { get; set; }
+
+
+    }
+}
